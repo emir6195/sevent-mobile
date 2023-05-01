e85ab44178639e29dc16304980d54f4703d4393f
Emir Şener <emir@cbot.ai>
2023-05-01 03:21:15 +0300

    login

--- lib/core/alerts.dart
+++ lib/core/alerts.dart
@@ -4,7 +4,11 @@ class Alerts {
   alertInfo(context, String text) {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-        content: Text(text),
+        content: Text(
+          text,
+          style: const TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.white12,
       ),
     );
   }
